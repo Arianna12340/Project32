8d4c5e5ddb9cc9cf95d775da77f2bb99d3c8c503
Arianna12340 <71414016+Arianna12340@users.noreply.github.com>
2020-11-29 17:19:12 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,172 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+var bg = "sprites/bg1.png";
+var score = 0;
+var backgroundImg;
+
+function preload() {
+  getBackgroundImg();
+}
+
+function setup() {
+  createCanvas(900,400);
+  engine = Engine.create();
+	world = engine.world;
+ ground = new Ground(450,380,900,20);
+ stand1 = new Stand(390,300,250,10);
+ stand2 = new Stand(700,200,200,10);
+
+ //first row
+box0 = new Box(300,275,30,40);
+ box1 = new Box(300,275,30,40);
+ box2 = new Box(330,275,30,40);
+ box3 = new Box(360,275,30,40);
+ box4 = new Box(390,275,30,40);
+ box5 = new Box(420,275,30,40);
+ box6 = new Box(450,275,30,40);
+ 
+
+//second row
+box7 = new Box(330,235,30,40);
+ box8 = new Box(360,235,30,40);
+ box9 = new Box(390,235,30,40);
+ box10 = new Box(420,235,30,40);
+ box11 = new Box(450.235,30,40);
+
+ //third row
+
+ box12 = new Box(360,195,30,40);
+ box13 = new Box(390,195,30,40);
+ box14 = new Box(420.195,30,40);
+
+ //fourth row
+ box15 = new Box(390.155,30,40);
+
+
+
+ b0 = new Box(640,175,30,40);
+ b1 = new Box(670,175,30,40);
+ b2 = new Box(700,175,30,40);
+ b3 = new Box(730,175,30,40);
+ b4 = new Box(760,175,30,40);
+
+ b5 = new Box(675,135,30,40);
+ b6 = new Box(700,135,30,40);
+ b7 = new Box(730,135,30,40);
+
+ b8 = new Box(700,95,30,40);
+
+ ball = Bodies.circle(50,200,20); 
+ World.add(world,ball); 
+ slingshot = new Slingshot(this.ball,{x:100,y:200});
+}
+
+function draw() {
+  if(backgroundImg)
+        background(backgroundImg);
+    
+        noStroke();
+        textSize(35)
+        fill("white")
+        text("Score  " + score, width-300, 50)
+
+  Engine.update(engine);
+  fill("brown")
+  ground.display(); 
+  fill("red")
+  stand1.display();
+  stand2.display();
+  fill("blue")
+  box0.display();
+  box1.display();
+  box2.display();
+  box3.display();
+  box4.display();
+  box5.display();
+  box6.display();
+
+  box0.score();
+  box1.score();
+  box2.score();
+  box3.score();
+  box4.score();
+  box5.score();
+  box6.score();
+  fill("green")
+  box7.display();
+  box8.display();
+  box9.display();
+  box10.display();
+  box11.display();
+
+  box7.score();
+  box8.score();
+  box9.score();
+  box10.score();
+  box11.score();
+  fill("yellow")
+  box12.display();
+  box13.display();
+
+  box12.score();
+  box13.score();
+  fill("white")
+  box14.display();
+  box14.score();
+
+  b0.display();
+  b1.display();
+  b2.display();
+  b3.display();
+  b4.display();
+
+  b0.score();
+  b1.score();
+  b2.score();
+  b3.score();
+  b4.score();
+  fill("lightblue")
+  b5.display();
+  b6.display();
+  b7.display();
+
+  b5.score();
+  b6.score();
+  b7.score();
+  fill("orange")
+  b8.display();
+  b8.score();
+
+  
+slingshot.display();
+ellipseMode(RADIUS);
+ellipse(ball.position.x,ball.position.y,20,20);
+
+  drawSprites();
+}
+function mouseDragged(){ 
+  Matter.Body.setPosition(this.ball,{x:mouseX,y:mouseY}); } 
+  function mouseReleased()
+  { slingshot.fly(); }
+
+  async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
+
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+    
+    if(hour>=06 && hour<=19){
+        bg = "bg1.png";
+    }
+    else{
+        bg = "bg2.jpg";
+    }
+
+    backgroundImg = loadImage(bg);
+    console.log(backgroundImg);
+}
